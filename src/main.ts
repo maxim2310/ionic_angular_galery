86aeb1943174381ed384133f3770e0ba8148340e
Maxim Kantsura <maxim@smoothr.de>
2024-03-17 17:51:39 +0200

    create swiper

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,20 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { provideHttpClient } from '@angular/common/http';
+import { register } from 'swiper/element/bundle';
 
 if (environment.production) {
   enableProdMode();
 }
+
+register();
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes),
+    provideHttpClient()
   ],
 });
